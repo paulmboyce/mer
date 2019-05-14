399445d3dc1b371b642355eecb8581d83519f4c8
paulmboyce <makeearthright@gmail.com>
2019-05-14 15:15:22 +0000

    WIP: REFACTOR CLEANUP AND INIT.

--- app.js
+++ app.js
@@ -7,30 +7,32 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-    var jsonData = {rss:"data is not yet loaded. Make async"};
-    
-    var parseString = xml2js.parseString;    
 
-    get.concat('https://www.esrl.noaa.gov/gmd/webdata/ccgg/trends/rss.xml', function (err, res, xmlData) {
-	if (err) throw err
-	console.log(res.statusCode) // 200
-	console.log(xmlData) // Buffer('this is the server response')
+// INIT DATA ON STARTUP
+// TODO: Refactor to daily.
+// ---------------------------------------
+var jsonData = {rss:"data is not yet loaded. Make async"};
+var parseString = xml2js.parseString;    
 
-	parseString(xmlData, function (err, result) {
-	    console.dir(result);
-	    console.log(util.inspect(result, false, null))
-	    jsonData = JSON.stringify(result);
-//	    console.log(jsonData);
-	    console.log("Parsed XML Data from https://www.esrl.noaa.gov/ feed [OK]");
-	    console.log("Waiting for requests..");
+get.concat('https://www.esrl.noaa.gov/gmd/webdata/ccgg/trends/rss.xml', function (err, res, xmlData) {
+    if (err) throw err
+    console.log(res.statusCode) // 200
+    console.log(xmlData) // Buffer('this is the server response')
 
-	});
-    })
+    parseString(xmlData, function (err, result) {
+	console.dir(result);
+	console.log(util.inspect(result, false, null))
+	jsonData = JSON.stringify(result);
+
+	console.log("Parsed XML Data from https://www.esrl.noaa.gov/ feed [OK]");
+	console.log("Waiting for requests..");
+
+    });
+})
 
 
 app.get('/', (req, res) => {
 
-//    console.log(JSON.stringify(jsonData));
 
     // TODO: ASYNC - because thisjsoData is not yet ready...
     console.log("1");
@@ -53,9 +55,6 @@ app.get('/', (req, res) => {
 			  
     res.send('\n\nHello World! The PPM this week is: XXX\n' + title + " ==> " + JSON.stringify( item)  );
 	
-
-
-
 })
        
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log('Example app listening on port ${port}!'));
