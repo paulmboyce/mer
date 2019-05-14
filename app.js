8aaf64e7da73bfe13426239088c321af09f3b5ad
paulmboyce <makeearthright@gmail.com>
2019-05-14 16:57:53 +0000

    BIG refactor to use better package for XML -> Javascript objects. Now reads cleanly

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const get = require('simple-get')
 const util = require('util')
 
-const xml2js = require('xml2js');
+var convert = require('xml-js');
 
 const express = require('express')
 const app = express()
@@ -11,49 +11,61 @@ const port = 3000
 // INIT DATA ON STARTUP
 // TODO: Refactor to daily.
 // ---------------------------------------
-var jsonData = {rss:"data is not yet loaded. Make async"};
-var parseString = xml2js.parseString;    
+
+var objData = {rss:'data is not yet loaded. Make async'};
 
 get.concat('https://www.esrl.noaa.gov/gmd/webdata/ccgg/trends/rss.xml', function (err, res, xmlData) {
     if (err) throw err
     console.log(res.statusCode) // 200
     console.log(xmlData) // Buffer('this is the server response')
 
-    parseString(xmlData, function (err, result) {
-	console.dir(result);
-	console.log(util.inspect(result, false, null))
-	jsonData = JSON.stringify(result);
+    // Print as JSON data (for info only)
+    var jsonData = convert.xml2json(xmlData, {compact: false, spaces: 4});
+    console.log ("jsonData ==> %s", jsonData);
 
-	console.log("Parsed XML Data from https://www.esrl.noaa.gov/ feed [OK]");
-	console.log("Waiting for requests..");
 
-    });
+    var options = {ignoreComment: true, alwaysChildren: true, compact:true, textKey: 'value', textFn: function(val) {return val.toUpperCase();}};
+    objData = convert.xml2js(xmlData, options);
+    console.log ("objData ==> %s", objData);
+    console.log ("objData.rss ==> %s", objData.rss);
+
+    console.log(Object.keys(objData.rss.channel));
+    
+    var rss = objData.rss;
+    console.log("\n\nRSS: ==> " + JSON.stringify(rss) );
+
+
+    var channel = rss.channel;
+    console.log("\n\nCHANNEL: ==> " + JSON.stringify(channel) );;
+
+    var title = channel.title;
+    console.log("TITLE: ==> %s \n\n" ,title );
+
+    console.log("About to print channel.item... \n");
+    console.log(channel.item);
+
+    console.log("About to print channel.item KEYs.. \n");
+    console.log(Object.keys(objData.rss.channel.item));
+
+    console.log("About to print channel.items.. \n");
+    var items= channel.item;
+
+    var i, item;
+    for (i = 0; i < items.length; i++) {
+	item = items[i];
+//	console.log("Item KEYS ==> [%s]", Object.keys(item));
+
+//	console.log(item);
+//	console.log("Item [%s] Description: [%s] \n\n", item.title.value, item.description.value); 
+    }
+    
 })
 
 
 app.get('/', (req, res) => {
 
 
-    // TODO: ASYNC - because thisjsoData is not yet ready...
-    console.log("1");
-	jsonData = JSON.parse(JSON.stringify(jsonData));
-
-    var parsedJson = JSON.parse(jsonData);
-
-
-    var rss = parsedJson.rss;
-    console.log("\n\nRSS: ==> " + JSON.stringify(rss) );
-
-    var channel = JSON.parse(JSON.stringify(rss.channel));
-    console.log("\n\nCHANNEL: ==> " + JSON.stringify(channel) );;
-
-// Channl is the colllection...
-    var title = channel[0].title;
-    console.log("TITLE: ==> " + title );
-
-    var item = channel[0];
-			  
-    res.send('\n\nHello World! The PPM this week is: XXX\n' + title + " ==> " + JSON.stringify( item)  );
+    res.send('\n\nHello World! The PPM this week is: XXX\n  ==> ' + JSON.stringify(objData.rss.channel.title)  );
 	
 })
        
