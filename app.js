7f1303c1a20ff89d9cc2d62c6d92e8af5b38a8ee
paulmboyce <makeearthright@gmail.com>
2019-05-21 16:52:19 +0000

    Baseline scaffolding. Needs to integrate the functional code form basic node app

--- app.js
+++ app.js
@@ -1,92 +1,41 @@
-const get = require('simple-get')
-const util = require('util')
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-var convert = require('xml-js');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const express = require('express')
-const app = express()
-const port = 3000
+var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Fiels to extract:
-const TITLE_DAILY_CO2_UPDATE = "DAILY CO2 UPDATE";
-const DESC_DAILY_AVERAGE_CO2_AT_MAUNA_LOA = "DAILY AVERAGE CO2 AT MAUNA LOA";
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-// INIT DATA ON STARTUP
-// TODO: Refactor to refresh hourly.
-// ---------------------------------------
-var objData = {rss:'data is not yet loaded. Make async'};
-var dailyAvgCO2 = "Not set";
-var dailyAvgCO2PubDate = "Not set";
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-get.concat('https://www.esrl.noaa.gov/gmd/webdata/ccgg/trends/rss.xml', function (err, res, xmlData) {
-    if (err) throw err
-    console.log(res.statusCode) // 200
-    console.log(xmlData) // Buffer('this is the server response')
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-    // Print as JSON data (for info only)
-    var jsonData = convert.xml2json(xmlData, {compact: false, spaces: 4});
-    console.log ("jsonData ==> %s", jsonData);
-
-
-    var options = {ignoreComment: true, alwaysChildren: true, compact:true, textKey: 'value', textFn: function(val) {return val.toUpperCase();}};
-    objData = convert.xml2js(xmlData, options);
-    console.log ("objData ==> %s", objData);
-    console.log ("objData.rss ==> %s", objData.rss);
-
-    console.log(Object.keys(objData.rss.channel));
-    
-    var rss = objData.rss;
-    console.log("\n\nRSS: ==> " + JSON.stringify(rss) );
-
-
-    var channel = rss.channel;
-    console.log("\n\nCHANNEL: ==> " + JSON.stringify(channel) );;
-
-    var title = channel.title;
-    console.log("TITLE: ==> %s \n\n" ,title );
-
-    console.log("About to print channel.item... \n");
-    console.log(channel.item);
-
-    console.log("About to print channel.item KEYs.. \n");
-    console.log(Object.keys(objData.rss.channel.item));
-
-    console.log("About to print channel.items.. \n");
-    var items= channel.item;
-
-
-    
-    var i, item;
-    for (i = 0; i < items.length; i++) {
-	item = items[i];
-	//	console.log("Item KEYS ==> [%s]", Object.keys(item));
-	//	console.log(item);
-
-	if (item.title.value.includes(TITLE_DAILY_CO2_UPDATE)  ) {
-	    var description = item.description.value;
-	    console.log("\nFOUND DAILY_AVERAGE_CO2_AT_MAUNA_LOA  [%s] \n\n", description);
-	    var idx = description.indexOf ("PPM");
-	    
-	    
-	    dailyAvgCO2PubDate = item.pubDate.value;
-	    dailyAvgCO2 = description.slice (idx -8 , idx -1).trim();
-	}
-	console.log("\nTitle ==> [%s] \nDescription ==> [%s] \n\n", item.title.value, item.description.value);
-	
-    }
-    console.log('\n\nThe latest daily PPM is:  ==> %s ppm [Published %s] \n\n',  dailyAvgCO2, dailyAvgCO2PubDate );
-    
-})
-
-
-app.get('/', (req, res) => {
-
-
-    res.send('\n\nThe latest daily CO2 PPM is:  ==>  ' +  dailyAvgCO2 + ' ppm \n\n'  );
-	
-})
-       
-app.listen(port, () => console.log('Example app listening on port ${port}!'));
+module.exports = app;
