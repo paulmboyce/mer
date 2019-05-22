1452b0c2f0b738dec2bb2b613c8018fe5dd98bdb
paulmboyce <makeearthright@gmail.com>
2019-05-22 11:06:41 +0000

    Refactored OK with Routr / Controller and ppm logic

---
+++ controllers/ppmController.js
@@ -0,0 +1,86 @@
+const get = require('simple-get')
+const util = require('util')
+
+var convert = require('xml-js');
+
+
+// Fiels to extract:
+const TITLE_DAILY_CO2_UPDATE = "DAILY CO2 UPDATE";
+const DESC_DAILY_AVERAGE_CO2_AT_MAUNA_LOA = "DAILY AVERAGE CO2 AT MAUNA LOA";
+
+// INIT DATA ON STARTUP
+// TODO: Refactor to refresh hourly.
+// ---------------------------------------
+var objData = {rss:'data is not yet loaded. Make async'};
+var dailyAvgCO2 = "Not set";
+var dailyAvgCO2PubDate = "Not set";
+
+
+get.concat('https://www.esrl.noaa.gov/gmd/webdata/ccgg/trends/rss.xml', function (err, res, xmlData) {
+    if (err) throw err
+    console.log(res.statusCode) // 200
+    console.log(xmlData) // Buffer('this is the server response')
+
+    // Print as JSON data (for info only)
+    var jsonData = convert.xml2json(xmlData, {compact: false, spaces: 4});
+    console.log ("jsonData ==> %s", jsonData);
+
+
+    var options = {ignoreComment: true, alwaysChildren: true, compact:true, textKey: 'value', textFn: function(val) {return val.toUpperCase();}};
+    objData = convert.xml2js(xmlData, options);
+    console.log ("objData ==> %s", objData);
+    console.log ("objData.rss ==> %s", objData.rss);
+
+    console.log(Object.keys(objData.rss.channel));
+    
+    var rss = objData.rss;
+    console.log("\n\nRSS: ==> " + JSON.stringify(rss) );
+
+
+    var channel = rss.channel;
+    console.log("\n\nCHANNEL: ==> " + JSON.stringify(channel) );;
+
+    var title = channel.title;
+    console.log("TITLE: ==> %s \n\n" ,title );
+
+    console.log("About to print channel.item... \n");
+    console.log(channel.item);
+
+    console.log("About to print channel.item KEYs.. \n");
+    console.log(Object.keys(objData.rss.channel.item));
+
+    console.log("About to print channel.items.. \n");
+    var items= channel.item;
+
+
+    
+    var i, item;
+    for (i = 0; i < items.length; i++) {
+	item = items[i];
+	//	console.log("Item KEYS ==> [%s]", Object.keys(item));
+	//	console.log(item);
+
+	if (item.title.value.includes(TITLE_DAILY_CO2_UPDATE)  ) {
+	    var description = item.description.value;
+	    console.log("\nFOUND DAILY_AVERAGE_CO2_AT_MAUNA_LOA  [%s] \n\n", description);
+	    var idx = description.indexOf ("PPM");
+	    
+	    
+	    dailyAvgCO2PubDate = item.pubDate.value;
+	    dailyAvgCO2 = description.slice (idx -8 , idx -1).trim();
+	}
+	console.log("\nTitle ==> [%s] \nDescription ==> [%s] \n\n", item.title.value, item.description.value);
+	
+    }
+    console.log('\n\nThe latest daily PPM is:  ==> %s ppm [Published %s] \n\n',  dailyAvgCO2, dailyAvgCO2PubDate );
+    
+});
+
+// Render latest daily CO2 PPM
+exports.ppm_daily = function(req, res) {
+//    res.send('\n\nCONTROLLER SAYS: The latest daily CO2 PPM is:  ==>  ' +  dailyAvgCO2 + ' ppm \n\n'  );
+    res.render('index', { title: 'The latest daily CO2 PPM is:  ' + dailyAvgCO2 });
+	
+};
+       
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+// Require controller modules.
+var ppm_controller = require('../controllers/ppmController');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', ppm_controller.ppm_daily);
 
 module.exports = router;
